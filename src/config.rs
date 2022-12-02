e47adb7fee8506c18972af7700ee0b7a4c3aeed5
fluffyChaos <23279594+fluffychaos@users.noreply.github.com>
2022-12-02 14:15:14 +0100

    Replaced mongodb with sqlite3

--- src/config.rs
+++ src/config.rs
@@ -1,18 +1,40 @@
-use std::env;
+use std::{env, path::{Path, PathBuf}};
 
-pub struct Config;
+pub struct Config {
+    discord_token: String,
+    bot_environment: String,
+    database_base_uri: String,
+    database_uri: PathBuf
+}
 
 impl Config {
     pub fn new() -> Config {
         dotenv::dotenv().expect("Loading .env failed");
-        Config
+        let discord_token = env::var("DISCORD_TOKEN").expect("Failed to load DISCORD_TOKEN");
+        let bot_environment = env::var("BOT_ENVIRONMENT").expect("Failed to load BOT_ENVIRONMENT");
+        let database_base_uri = env::var("DATABASE_BASE_URI").expect("Failed to load MONGO_URI");
+        let database_uri = Path::new(&database_base_uri).join(format!("{}.sqlite", &bot_environment));
+        Config {
+            discord_token,
+            bot_environment,
+            database_base_uri,
+            database_uri
+        }
     }
 
-    pub fn discord_token(&self) -> String {
-        env::var("DISCORD_TOKEN").expect("Failed to load DISCORD_TOKEN")
+    pub fn discord_token(&self) -> &str {
+        &self.discord_token
     }
 
-    pub fn bot_environment(&self) -> String {
-        env::var("BOT_ENVIRONMENT").expect("Failed to load BOT_ENVIRONMENT")
+    pub fn bot_environment(&self) -> &str {
+        &self.bot_environment
+    }
+
+    pub fn database_base_uri(&self) -> &str {
+        &self.database_base_uri
+    }
+
+    pub fn database_uri(&self) -> &Path {
+        &self.database_uri
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -60,18 +60,17 @@ async fn main() {
         data.insert::<ShardManagerContainer>(client.shard_manager.clone());
     }
     let shard_manager = client.shard_manager.clone();
-
     tokio::spawn(async move {
         tokio::signal::ctrl_c()
             .await
             .expect("Could not register ctrl+c handler");
         shard_manager.lock().await.shutdown_all().await;
     });
-    println!("Hello, world!");
     println!(
         "The Lobbymaker bot runs in the {} environment",
         config.bot_environment()
     );
+    let database = rusqlite::Connection::open(config.database_uri()).expect(format!("Unable to open the {} database", config.bot_environment()).as_str());
     if let Err(err) = client.start().await {
         println!("Client error: {:?}", err);
     }
